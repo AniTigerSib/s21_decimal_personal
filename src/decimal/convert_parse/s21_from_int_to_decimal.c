e034732f7e5d408409b1058c1659611a0ed2f123
Michael <khudoleev04@mail.ru>
2024-09-08 21:09:14 +0500

    decimal/Added DOxygen doc for 'from_int_to_decimal' function, also some local changes

--- src/decimal/convert_parse/s21_from_int_to_decimal.c
+++ src/decimal/convert_parse/s21_from_int_to_decimal.c
@@ -5,12 +5,39 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * @brief Converts an integer to a decimal number.
+ *
+ * This function converts the input integer to a decimal number and stores it in the
+ * provided s21_decimal structure.
+ *
+ * @param src The integer to be converted.
+ * @param dst Pointer to the s21_decimal structure that will store the decimal number.
+ *
+ * @return The status of the operation, which can be COMMON_OK or ERR.
+ */
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-  common_status_e status = (dst == NULL) * ERR;
+  /**
+   * @brief Status of the operation.
+   */
+  common_status_e status = COMMON_OK;
+
+  /**
+   * @brief Temporary s21_decimal_u structure to store the decimal number.
+   */
   s21_decimal_u *val = (s21_decimal_u *)dst;
+
+  /**
+   * @brief Checks if there is NULL pointer given as input parameter.
+   */
+  status = (dst == NULL) * ERR;
+
+  /**
+   * @brief Converts the integer to a decimal number.
+   */
   if (status == COMMON_OK) {
     val->binary.sign = (src < 0) * NEGATIVE;
-    
+
     if (src == INT_MIN) {
       val->binary.bits[0] = (uint32_t) INT_MAX + 1;
       src = 0;
@@ -24,5 +51,9 @@ int s21_from_int_to_decimal(int src, s21_decimal *dst) {
       src /= 10;
     }
   }
+
+  /**
+   * @return The status of the operation.
+   */
   return status;
 }
